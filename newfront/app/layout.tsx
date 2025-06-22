c72d8ef8b5f95defe75389ceec169cadcff46ef5
Thomson-Lam <thomson.lam327@gmail.com>
2025-06-22 08:00:59 -0500

    tidied up frontend

--- newfront/app/layout.tsx
+++ newfront/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'AdLumen: The AI Ad Killer',
+  description: 'Join the AdLumenati', // Illumenadti
+  generator: 'v0.dev + Thomson Lam on 2 hours of sleep',
 }
 
 export default function RootLayout({
